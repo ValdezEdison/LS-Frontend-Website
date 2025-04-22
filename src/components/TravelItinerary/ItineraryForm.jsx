8a927939c7e59210bc87c0ff00a18740bd5e9a46
Anu T <anut@zoondia.net>
2025-04-22 15:34:47 +0530

    design fix in My trips

--- src/components/TravelItinerary/ItineraryForm.jsx
+++ src/components/TravelItinerary/ItineraryForm.jsx
@@ -3,48 +3,56 @@ import styles from "./ItineraryForm.module.css";
 
 const ItineraryForm = () => {
   return (
+    <>
     <form className={styles.itineraryForm}>
-      <div className={styles.formGroup}>
-        <label htmlFor="tripName" className={styles.label}>
-          Nombre del viaje
-        </label>
-        <input
-          type="text"
-          id="tripName"
-          className={styles.input}
-          value="Viaje cumple mamá"
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="tripDates" className={styles.label}>
-          Fechas
-        </label>
-        <div className={styles.dateInput}>
-          <span className={styles.calendarIcon} />
+      <div className={styles.itineraryFormInputsWrapper}>
+        <div className={styles.formGroup}>
+          <label htmlFor="tripName" className={styles.label}>
+            Nombre del viaje
+          </label>
           <input
             type="text"
-            id="tripDates"
+            id="tripName"
             className={styles.input}
-            value="24/01/2025 - 30/01/2025"
+            value="Viaje cumple mamá"
           />
         </div>
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="destination" className={styles.label}>
-          Destino
-        </label>
-        <div className={styles.destinationInput}>
-          <input
-            type="text"
-            id="destination"
-            className={styles.input}
-            value="París"
-          />
-          <button type="button" className={styles.deleteButton}>
-            Eliminar
-          </button>
+        <div className={styles.formGroup}>
+          <label htmlFor="tripDates" className={styles.label}>
+            Fechas
+          </label>
+          <div className={styles.dateInput}>
+            <span className={styles.calendarIcon} />
+            <input
+              type="text"
+              id="tripDates"
+              className={styles.input}
+              value="24/01/2025 - 30/01/2025"
+            />
+          </div>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="destination" className={styles.label}>
+            Destino
+          </label>
+          <div className={styles.destinationInput}>
+            <div className={styles.destinationFormInput}>
+              <span className={styles.searchIcon} />
+              <input
+                type="text"
+                id="destination"
+                className={styles.input}
+                value="París"
+              />
+            </div>
+            
+            <button type="button" className={styles.deleteButton}>
+              Eliminar
+            </button>
+          </div>
         </div>
       </div>
+     
       <div className={styles.formActions}>
         <button type="button" className={styles.cancelButton}>
           Cancelar
@@ -53,10 +61,12 @@ const ItineraryForm = () => {
           Guardar
         </button>
       </div>
-      <button type="button" className={styles.addDestinationButton}>
-        Añadir destino
-      </button>
+     
     </form>
+     <button type="button" className={styles.addDestinationButton}>
+     Añadir destino
+   </button>
+   </>
   );
 };
 
