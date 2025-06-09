3a318cb60e9b53d9d6f2810a9de469bcc1f503f5
vivekbabu <vivekbabu@zooservers.com>
2025-06-09 18:59:39 +0530

    updated contactUs service and newsletter service

--- src/features/common/newsLetter/NewsLetterService.jsx
+++ src/features/common/newsLetter/NewsLetterService.jsx
@@ -17,38 +17,18 @@ const getCookie = (name) => {
     return cookieValue;
   };
 
-  console.log('Native document.cookie:', document.cookie);
-
-
-console.log('Cookies.get():', Cookies.get());
-console.log('Cookies.get("csrftoken"):', Cookies.get('csrftoken'));
-console.log('Cookies.get("csrftoken", { path: "/" }):', Cookies.get('csrftoken', { path: '/' }));
-
-function getCookieValue(name) {
-  const cookies = document.cookie.split(';');
-  for (let c of cookies) {
-    const [key, value] = c.trim().split('=');
-    if (key === name) return decodeURIComponent(value);
-  }
-  return null;
-}
 
 
 
   const NewsLetterService = {
     subscribe: async (email, gdpr_consent) => {
         const csrfToken = Cookies.get('csrftoken');
-        console.log('CSRF Token:', csrfToken);
-        console.log('document.cookie:', document.cookie);
-        console.log('Manual getCookieValue:', getCookieValue('csrftoken'));
-        console.log('Cookies.get():', Cookies.get());
-        console.log('Cookies.get("csrftoken"):', Cookies.get('csrftoken'));
-        console.log('Cookies.get("csrftoken", { path: "/" }):', Cookies.get('csrftoken', { path: '/' }));
+        
         return ApiService.post("/newsletter/subscribe/", 
           { email, gdpr_consent },
           {
             headers: {
-              'X-CSRFToken': 'B0cGPFoDUP1lxGCADY2pYjj0rCKSQ8EQ',
+              'X-CSRFToken': csrfToken,
             }
           }
         );
