cd796441724f2b41c3b3a67a24ff9413313a3fc9
MRVLDZ <iancasillasbuffon@gmail.com>
2025-06-30 11:26:28 +0200

    Added the click to contact page

--- src/components/footerBlockPages/helpCenter/ContactSection.jsx
+++ src/components/footerBlockPages/helpCenter/ContactSection.jsx
@@ -5,23 +5,30 @@ import { useTranslation } from 'react-i18next';
 const ContactSection = () => {
     const { t } = useTranslation("HelpCenter");
     
+    const handleContactClick = () => {
+      window.location.href = "/contact";
+    };
+
     return (
       <footer className={styles.contactWrapper}>
-        <section className={styles.contactSection}>
-          <div className="page-center">
-            <div className={styles.contactContent}>
-              <h2 className={styles.contactTitle}>{t('helpCenter.contactTitle')}</h2>
-              <p className={styles.contactDescription}>
-                {t('helpCenter.contactDescription')}
-              </p>
-            </div>
-            <div className={styles.buttonWrapper}>
-              <button className={styles.contactButton}>
-                {t('helpCenter.contactButton')}
-              </button>
-            </div>
-          </div>
-        </section>
+      <section className={styles.contactSection}>
+        <div className="page-center">
+        <div className={styles.contactContent}>
+          <h2 className={styles.contactTitle}>{t('helpCenter.contactTitle')}</h2>
+          <p className={styles.contactDescription}>
+          {t('helpCenter.contactDescription')}
+          </p>
+        </div>
+        <div className={styles.buttonWrapper}>
+          <button
+          className={styles.contactButton}
+          onClick={handleContactClick}
+          >
+          {t('helpCenter.contactButton')}
+          </button>
+        </div>
+        </div>
+      </section>
       </footer>
     );
   };
